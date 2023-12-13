a244cbddf2cdeef34f542abc33db90f766d6c20d
katreddyvenkateswaraReddy <katreddy41@gmail.com>
2023-12-13 15:22:26 +0530

    New Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import "./styles/App.css"
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
